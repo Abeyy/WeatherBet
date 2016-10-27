8fa6e96e3b6152f1072bf29057095fd1f23bf6bd
J L <jl4722@nyu.edu>
2016-10-27 16:10:45 -0400

    blah...

--- Weatherbet/config/routes.rb
+++ Weatherbet/config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   #set the index page
   root 'pages#home'
 
-  
+
+
   get 'pages/home'
 
   get 'pages/weather'
 
   get 'pages/locations'
+
+  get '/~jl4722/wbRails' to 'pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
