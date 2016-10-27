08dbbaab618aba0b493760d8374f4978747256e9
Abey Joy <Abey.joy@nyu.edu>
2016-10-27 18:56:56 -0400

    routes bug fixed

--- Weatherbet/config/routes.rb
+++ Weatherbet/config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   get 'pages/locations'
 
-  get '/~jl4722/wbRails' to 'pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
