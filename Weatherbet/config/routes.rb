58d25f305f4b9a9959a9ef263dc271c7315f18e1
J L <jl4722@nyu.edu>
2016-11-26 12:50:21 -0500

    replace routes.rb

---
+++ Weatherbet/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  
+  resources :locations
+  resources :reports
+  resources :predictions
+  #set the index page
+  root 'pages#home'
+
+  get 'pages/home'
+
+  get 'pages/weather'
+
+  get 'pages/locations'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
