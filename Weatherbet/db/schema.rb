81c80b4f310d0c8d6c05c8936cdf599aac4d472a
J L <jl4722@nyu.edu>
2016-11-26 00:57:23 -0500

    add userid colum to prediction and report

--- Weatherbet/db/schema.rb
+++ Weatherbet/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126040531) do
+ActiveRecord::Schema.define(version: 20161126055635) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -24,7 +24,9 @@ ActiveRecord::Schema.define(version: 20161126040531) do
     t.integer  "location_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
     t.index ["location_id"], name: "index_predictions_on_location_id"
+    t.index ["user_id"], name: "index_predictions_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -33,7 +35,9 @@ ActiveRecord::Schema.define(version: 20161126040531) do
     t.integer  "location_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
     t.index ["location_id"], name: "index_reports_on_location_id"
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
