55a23c54dc664a074adb43cf96164d3169b89d00
J L <jl4722@nyu.edu>
2016-11-26 02:30:27 -0500

    add automatic weight calculation for the logged in user

--- Weatherbet/app/controllers/pages_controller.rb
+++ Weatherbet/app/controllers/pages_controller.rb
@@ -4,12 +4,37 @@ class PagesController < ApplicationController
 
   def weather
   	require "#{Rails.root}/Cstuff/rice_test"
-  	# @stat = RiceTest.new
-  	# @stat = WbStat.new
+
+    @user = current_user
+    if user_signed_in?    
+      @usrStats = WbStat.new 
+      @usrStats.correct=0
+
+      @predictions = @user.predictions
+      @usrStats.total = @predictions.count
+
+      @predictions.each do |p|
+        isTrue = false
+        Report.where(location: p.location).each do |r|
+          sINr =(r.start < p.start and p.start < r.end)
+          eINr =(r.start < p.end and p.start < r.end)
+          isTrue = (isTrue or sINr or eINr)
+        end #end Report.wher... do |r|
+
+        if isTrue 
+          @usrStats.correct += 1
+        end #end isTrue
+
+      end #end predictions.each do |p|
+
+    end #end if user_signed_in
+
+  	# @usrStats = RiceTest.new
+  	# @usrStats = WbStat.new
   	# require '/Users/_-_/Documents/Poly/Poly Fall 2016/Software EG/Weatherbet/untitled folder/WeatherBet-Rails-/Weatherbet/Cstuff/rice_test'
-	# @stat = WbStat.new
+	# @usrStats = WbStat.new
 	# @blah = RiceTest.new
-	# @stat.total = 12
+	# @usrStats.total = 12
   end
 
   def locations
