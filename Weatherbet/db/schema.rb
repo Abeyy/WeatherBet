44c2e1bb8195933d518140d0b0fc22824e5f0a45
Abey Joy <Abey.joy@nyu.edu>
2016-11-30 22:12:30 -0500

    new db

--- Weatherbet/db/schema.rb
+++ Weatherbet/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126055635) do
+ActiveRecord::Schema.define(version: 20161129224622) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.decimal  "lat",        precision: 10, scale: 10
+    t.decimal  "lng",        precision: 10, scale: 10
   end
 
   create_table "predictions", force: :cascade do |t|
@@ -53,6 +55,7 @@ ActiveRecord::Schema.define(version: 20161126055635) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.float    "weight"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
